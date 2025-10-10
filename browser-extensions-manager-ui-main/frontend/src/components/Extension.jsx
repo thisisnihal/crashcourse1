0659bea1455f1fd9b7dbf1733dc8ec5959016273
Nihal Kumar <nihal.kumar@msg-global.com>
2025-10-10 14:48:40 +0530

    Extension Card Updated

--- browser-extensions-manager-ui-main/frontend/src/components/Extension.jsx
+++ browser-extensions-manager-ui-main/frontend/src/components/Extension.jsx
@@ -3,11 +3,11 @@ import ExtensionCard from "./ExtensionCard";
 import icon from "../assets/images/logo-devlens.svg"
 
 
-function Button({text, active=false}) {
+function Button({ text, active = false }) {
 
-    return <button className={`cursor-pointer rounded-2xl backdrop-blur-lg bg-accent/60 shadow-md pr-4 pl-4 pt-0.5 pb-0.5  ${active ? "bg-red-500 text-bg" : "text-text"}`}>
-        {text}
-    </button>
+  return <button className={`cursor-pointer rounded-2xl backdrop-blur-lg bg-accent/60 shadow-md pr-4 pl-4 pt-0.5 pb-0.5  ${active ? "bg-red-500 text-bg" : "text-text"}`}>
+    {text}
+  </button>
 }
 
 function Extension() {
@@ -15,15 +15,15 @@ function Extension() {
     <div className="flex flex-col w-full h-full mt-4 text-text p-1 gap-8 ">
       <div className="flex justify-between">
         <h1 className="text-2xl font-bold ">Extensions List</h1>
-        <span className="flex w-fit gap-16 justify-evenly">
-           <Button text={"All"} active={true} />
-            <Button text={"Active"} />
-             <Button text={"InActive"} />
+        <span className="flex w-fit gap-6 justify-evenly">
+          <Button text={"All"} active={true} />
+          <Button text={"Active"} />
+          <Button text={"InActive"} />
         </span>
       </div>
 
       <div>
-            <ExtensionCard icon={icon} title={"DevLens"} description={"Quickly inspect page layouts and visualize element boundaries."} active={true} />
+        <ExtensionCard icon={icon} title={"DevLens"} description={"Quickly inspect page layouts and visualize element boundaries."} active={true} />
       </div>
     </div>
   );
--- browser-extensions-manager-ui-main/frontend/src/components/ExtensionCard.jsx
+++ browser-extensions-manager-ui-main/frontend/src/components/ExtensionCard.jsx
@@ -2,16 +2,21 @@ import React from 'react'
 
 function ExtensionCard({icon, title, description, active}) {
   return (
-    <div className='w-1/3 h-40 flex flex-col justify-between cursor-pointer rounded-2xl backdrop-blur-lg bg-accent/60 shadow-md pr-1 pl-2.5 pt-0.5 pb-0.5 text-sm text-wrap'>
-        <span className='flex justify-between gap-2 item-baseline'>
+    <div className='w-1/3 h-48 flex flex-col justify-between cursor-pointer rounded-2xl backdrop-blur-lg bg-accent/60 shadow-md p-5 text-sm text-wrap'>
+        <span className='flex justify-between gap-4 item-baseline'>
             <img src={icon} alt="" />
             <span className='flex flex-col '>
                 <h1 className='font-bold text-2xl'>{title}</h1>
                 <p>{description}</p>
             </span>
         </span>
-        <span>
+          
+        <span className='flex justify-between'>
+    <button className='cursor-pointer hover:bg-red-400 hover:text-accent  rounded-2xl backdrop-blur-lg bg-accent/60 border-2 pt-1 pb-1 pr-2.5 pl-2.5'>
+      Remove
+    </button>
 
+    
         </span>
     </div>
   )
